e2de1b0cf55c63ecde6fd6b34e73605586792ceb
Nithya Sam <nithyasam@nithyas-air.gateway.pace.com>
2017-09-07 19:01:38 -0500

    added the models and routes

---
+++ public/app.js
@@ -0,0 +1,23 @@
+$.get("/articles", function(data) {
+  for (var i = 0; i < data.length; i++) {
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link +"</p>" + 
+      "<button class='savebtn' savebtn-id=" + data[i]._id +">Save Article</button>");
+  }
+});
+$(document).on("click", "#scrape", function() {
+  $.ajax({
+    method: "GET",
+    url: "/scrape"
+  }).done(function(data){
+   alert("scrape complete");
+ });
+});
+$(document).on("click", ".savebtn", function() {
+  var thisId = $(this).attr("savebtn-id");
+  $.ajax({
+    method: "POST",
+    url: "/saveArticle/" + thisId,
+  }).done(function(data) {
+    location.reload();
+  });
+});
